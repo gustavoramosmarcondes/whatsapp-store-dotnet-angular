1dd0a63af1ebab099764468a194843159f54c209
gustavoramosmarcondes <gustavo.ramos.marcondes@gmail.com>
2026-01-15 19:08:58 -0300

    Criada interface de repositório de produtos

--- backend/WhatsappStore.Domain/Entities/Product.cs
+++ backend/WhatsappStore.Domain/Entities/Product.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 
-public class Class1
+namespace WahtsappStore.Domain.Entities
 {
-	public Class1()
+	public class Class1
 	{
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		public int Stock { get; set; }	
+		public bool IsActive { get; set; }
+		public DateTime Created { get; set; }
+
 	}
 }
