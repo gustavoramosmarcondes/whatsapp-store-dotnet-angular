554a76c64196844c3664f8f769126adf13dd913b
gustavoramosmarcondes <gustavo.ramos.marcondes@gmail.com>
2026-01-16 15:52:50 -0300

    Criada interface de repositório de produtos

---
+++ backend/WhatsappStore.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WhatsappStore.Application.Interfaces
+{
+	public interface IProductRepository()
+	{
+		// Retorna todos os produtos
+		Task<IEnumerable<Product>> GetAll();
+
+		// Retorna um produto pelo ID
+		Task<Product> GetById(int id);
+
+        // Adiciona um novo produto
+		Task<Product> Add(Product product);
+
+       
+    }
+}
